46a4424d10b63cd4468e6fcb04c39d59a4cfeb92
Dheeraj Kumar <dk8549644@gmail.com>
2025-09-01 10:16:57 +0530

    session_1 bash command

---
+++ os/session1/prog3.sh
@@ -0,0 +1,16 @@
+day=""
+
+echo Enter a day
+
+read day
+
+case $day in 
+mon) echo "Formal Dress";;
+tue) echo "Semi Formal Dress";;
+wed) echo "Casual Dress";;
+thur) echo "Formal Dress Dress";;
+fir) echo "Funky Dress";;
+sat) echo "Holiday";;
+sun) echo "Holiday";;
+
+esac
